85bc41232b8990fa05dd358a25f9643dd491fb19
tamajit-ghosh-13 <tamajit.ghosh.13@gmail.com>
2026-05-03 14:44:56 +0530

    feat: implement Clerk authentication routes and configure middleware for route protection and redirection

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,39 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/home",
+]);
+
+const isPublicApiRoutes = createRouteMatcher(["/api/webhooks(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  const currentUrl = new URL(req.url);
+
+  const isAccessingDashboard = currentUrl.pathname === "\home";
+  const isApiRequest = currentUrl.pathname.startsWith("/api");
+
+  // loggedIn yet trying to access public routes
+  if (userId && isPublicRoute(req) && !isAccessingDashboard) {
+    return NextResponse.redirect(new URL("/home", req.url));
+  }
+
+  // not loggedIn
+  if (!userId) {
+    // not loggedIn trying to access private routes
+    if (!isPublicRoute(req) && !isPublicApiRoutes(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+    // not loggedIn trying to access api routes
+    if (isApiRequest && !isPublicApiRoutes(req)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  }
+});
 
 export const config = {
   matcher: [
